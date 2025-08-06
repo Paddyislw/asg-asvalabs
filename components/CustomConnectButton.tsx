c9d80588d2f731736c394433b080b7bfe5137c6e
Paddyislw <pradyum0701@gmail.com>
2025-08-07 00:10:23 +0530

    Fixed the ui for the wrong network button

--- components/CustomConnectButton.tsx
+++ components/CustomConnectButton.tsx
@@ -52,7 +52,7 @@ export const CustomConnectButton = () => {
                 return (
                   <button
                     onClick={openChainModal}
-                    className="px-4 py-2 rounded-full border border-red-500 text-red-500  hover:bg-red-500  transition-colors"
+                    className="px-4 py-2 rounded-full border border-red-500 text-red-500  hover:bg-red-500 hover:text-white transition-colors"
                   >
                     Wrong network
                   </button>
